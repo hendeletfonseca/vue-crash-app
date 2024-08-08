25bca6e3b16ecc30e78a9fd27404e5335b8f3393
Hendel Fonseca <hendeletfonseca@gmail.com>
2024-08-07 21:38:21 -0300

    feat: add job and jobListings

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,12 @@
 import Navbar from '@/components/Navbar.vue';
 import Hero from '@/components/Hero.vue';
 import HomeCards from '@/components/HomeCards.vue';
+import JobListings from './components/JobListings.vue';
 </script>
 
 <template>
     <Navbar />
     <Hero title="Changed title" />
     <HomeCards />
+    <JobListings :limit="3" :show-button="true" />
 </template>
